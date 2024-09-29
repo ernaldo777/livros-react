3f3710db5f2120caf045ecd65fa5d36ef7405373
ERNALDO VIEIRA <69253807+ernaldo777@users.noreply.github.com>
2024-09-29 00:10:50 -0300

    Add files via upload

--- src/controle/ControleLivros.ts
+++ src/controle/ControleLivros.ts
@@ -5,6 +5,7 @@ const livros: Array<Livro> = [
   { codigo: 1, codEditora: 1, titulo: 'Livro 1', resumo: 'Resumo do Livro 1', autores: ['Autor 1'] },
   { codigo: 2, codEditora: 2, titulo: 'Livro 2', resumo: 'Resumo do Livro 2', autores: ['Autor 1', 'Autor 2'] },
   { codigo: 3, codEditora: 3, titulo: 'Livro 3', resumo: 'Resumo do Livro 3', autores: ['Autor 1', 'Autor 2', 'Autor 3'] },
+  { codigo: 4, codEditora: 3, titulo: 'Livro 4', resumo: 'Resumo do Livro 4', autores: ['Autor 4', 'Autor 5'] }
 ];
 
 class ControleLivro {
